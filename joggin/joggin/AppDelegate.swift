5fdb6842af05c83d881ba27a42dbfde6ac2d91d1
Victor Gabriel Chirino <vicchirino@gmail.com>
2015-03-04 15:42:05 -0300

    Add parse framework

--- joggin/joggin/AppDelegate.swift
+++ joggin/joggin/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        Parse.setApplicationId("p9uVPpupjkiPmn35pDmH5X4JwWeOkVKEvJs3rRyT", clientKey: "62jzzhx0RfAo94yQMBGuKr45chck57XYn1o9OC0z")
+        
         return true
     }
 
