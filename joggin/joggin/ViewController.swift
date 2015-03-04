90ca847a9bdbcc23d73bc7d8b1259ce7b6912043
Victor Gabriel Chirino <vicchirino@gmail.com>
2015-03-04 14:02:46 -0300

    project commit

---
+++ joggin/joggin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  joggin
+//
+//  Created by Victor Chirino on 3/4/15.
+//  Copyright (c) 2015 Victor Chirino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
